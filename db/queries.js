0ccd3d8a1c1156ed6274645bd86e99a1ba36eb55
Cassandra Torske <cassandra.torske@gmail.com>
2017-12-06 19:57:24 -0700

    limit on queries

--- db/queries.js
+++ db/queries.js
@@ -5,16 +5,16 @@ module.exports = {
     return knex('comment').orderBy('created_at', 'desc').limit(5)
   },
   getAll2(){
-    return knex('comment').orderBy('created_at', 'desc').offset(5)
+    return knex('comment').orderBy('created_at', 'desc').offset(5).limit(6)
   },
   getAll3(){
-    return knex('comment').orderBy('created_at', 'desc').offset(10)
+    return knex('comment').orderBy('created_at', 'desc').offset(10).limit(6)
   },
   getAll4(){
-    return knex('comment').orderBy('created_at', 'desc').offset(15)
+    return knex('comment').orderBy('created_at', 'desc').offset(15).limit(6)
   },
   getAll5(){
-    return knex('comment').orderBy('created_at', 'desc').offset(20)
+    return knex('comment').orderBy('created_at', 'desc').offset(20).limit(6)
   },
   create(comment) {
     return knex('comment').insert(comment, '*')
