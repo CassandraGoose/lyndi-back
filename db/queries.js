e47038441241dc75e67b0cc55f2681524a0dfacb
Cassandra Torske <cassandra.torske@gmail.com>
2017-12-06 18:42:24 -0700

    pagination prep

--- db/queries.js
+++ db/queries.js
@@ -4,6 +4,18 @@ module.exports = {
   getAll() {
     return knex('comment').orderBy('created_at', 'desc')
   },
+  getAll2(){
+    return knex('comment').orderBy('created_at', 'desc').offset(5)
+  },
+  getAll3(){
+    return knex('comment').orderBy('created_at', 'desc').offset(10)
+  },
+  getAll4(){
+    return knex('comment').orderBy('created_at', 'desc').offset(15)
+  },
+  getAll5(){
+    return knex('comment').orderBy('created_at', 'desc').offset(20)
+  },
   create(comment) {
     return knex('comment').insert(comment, '*')
   },
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,26 @@ router.get('/', async(req, res) => {
   res.json(comments)
 })
 
+router.get('/page-2', async(req, res) => {
+  let comments = await queries.getAll2()
+  res.json(comments)
+})
+
+router.get('/page-3', async(req, res) => {
+  let comments = await queries.getAll3()
+  res.json(comments)
+})
+
+router.get('/page-4', async(req, res) => {
+  let comments = await queries.getAll4()
+  res.json(comments)
+})
+
+router.get('/page-5', async(req, res) => {
+  let comments = await queries.getAll5()
+  res.json(comments)
+})
+
 router.post('/', async function(req, res) {
   let comment = {
     name: req.body.name,
